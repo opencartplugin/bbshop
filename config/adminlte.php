a09fa6261f7ba5772cf25afce57902389d20e3a2
Muhammad Farid Saleh <opencartplugin@gmail.com>
2019-03-19 16:16:29 +0100

    tes1

---
+++ config/adminlte.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'Theme' => [
+        'title' => 'Barber Shop',
+        'logo' => [
+            'mini' => '<b>BShop</b>',
+            'large' => '<b>BarberShop</b>'
+        ],
+        'login' => [
+            'show_remember' => false,
+            'show_register' => false,
+            'show_social' => false
+        ],
+        'folder' => ROOT,
+        'skin' => 'blue'
+    ]
+];
+?>
